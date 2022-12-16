df0bd2660c082346644b77c957c2efbbb336a226
marivalerio <69995637+marivalerio@users.noreply.github.com>
2022-12-16 08:55:40 -0300

    Delete projeto-viva-api directory

--- projeto-viva-api/src/main/java/com/projeto/viva/enums/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.projeto.viva.enums;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADM;
-}
--- projeto-viva-api/src/main/java/com/projeto/viva/services/CustomUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.viva.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomUserDetailsService {
-}
